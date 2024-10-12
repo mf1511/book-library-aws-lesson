e3aa6074f411a68db79ec09afd162dac0990be48
marin <marin1511@hotmail.fr>
2024-10-12 11:56:08 +0200

    setup and settings

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { add } from '@parking-application-monorepo/sample-lib'
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Hello Woffrld! ${add(12, 23)}`
   }
 }
